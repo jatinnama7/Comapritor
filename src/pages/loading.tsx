86dedc5b91a78c3cc585986c2e491e210c8829eb
Sarthak Sain <Sarthaksain08@gmail.com>
2025-06-06 11:48:14 +0530

    loading animation

---
+++ src/pages/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlankPage = () => {
+  return (
+    <div className="w-screen h-screen bg-white">
+      {/* Blank Page */}
+    </div>
+  );
+};
+
+export default BlankPage;
