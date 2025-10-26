06d74ad6bf68773c6d29c9c00effc5f212d9c51b
Sarthak Sain <Sarthaksain08@gmail.com>
2025-10-26 10:40:34 +0530

    in config

--- backend/login/config/db.js
+++ backend/login/config/db.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      // "mongodb+srv://sarthaksain:comparitor@finalyearproject.bo0vvuf.mongodb.net/?retryWrites=true&w=majority&appName=Finalyearproject",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
