b20193277ef36e1c0c0ea67209f4c40fddfa0c75
Sarthak Sain <Sarthaksain08@gmail.com>
2025-10-26 10:35:32 +0530

    ch

---
+++ backend/login/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      // "mongodb+srv://sarthaksain:comparitor@finalyearproject.bo0vvuf.mongodb.net/?retryWrites=true&w=majority&appName=Finalyearproject",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("✅ Connected to MongoDB ");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
